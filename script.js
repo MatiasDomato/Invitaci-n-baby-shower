131d2253347bb7592d66ffa3bed92d52d5328ba1
Matias_Domato <matiasdomato97@gmail.com>
2023-12-26 13:21:56 -0300

    solution responsive 1.1

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const jsConfetti = new JSConfetti();
-
-document
-.querySelector("#congrats")
-.addEventListener("click", (e) => {
-    jsConfetti.addConfetti();
-});
+document.addEventListener("DOMContentLoaded", function () {
+    // Espera a que el DOM esté completamente cargado
+  
+    const jsConfetti = new JSConfetti();
+  
+    // Obtén el botón por su ID
+    var congratsButton = document.getElementById("congrats");
+  
+    // Agrega un event listener al botón
+    congratsButton.addEventListener("click", function () {
+      // Lanza la lluvia de confeti
+      jsConfetti.addConfetti();
+  
+      // Muestra una alerta interactiva
+      Swal.fire({
+        title: '¡Gracias por confirmar tu asistencia!',
+        text: 'Te redirigimos a WhatsApp asi confirmas tu asistencia 😎',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+      }).then((result) => {
+        // Redirige a WhatsApp si hacen clic en "Aceptar"
+        if (result.isConfirmed) {
+          window.location.href = 'https://api.whatsapp.com/send?phone=+5491132620544&text=Hola,%20quiero%20confirmar%20mi%20asistencia%20al%20Baby%20Shower.';
+        }
+      });
+    });
+  });
+  
+  
+  
