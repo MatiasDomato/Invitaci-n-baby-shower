83f51e211aa05f70e9025826c790ebc968546231
Matias_Domato <matiasdomato97@gmail.com>
2023-12-27 14:59:19 -0300

    solution responsive 1.2

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Espera a que el DOM esté completamente cargado
-  
-    const jsConfetti = new JSConfetti();
-  
-    // Obtén el botón por su ID
-    var congratsButton = document.getElementById("congrats");
-  
-    // Agrega un event listener al botón
-    congratsButton.addEventListener("click", function () {
-      // Lanza la lluvia de confeti
-      jsConfetti.addConfetti();
-  
+  // Espera a que el DOM esté completamente cargado
+
+  const jsConfetti = new JSConfetti();
+
+  // Obtén el botón por su ID
+  var congratsButton = document.getElementById("congrats");
+
+  // Agrega un event listener al botón
+  congratsButton.addEventListener("click", function () {
+    // Lanza la lluvia de confeti
+    jsConfetti.addConfetti();
+
+    // Espera 2200 ms (2.2 segundos) antes de mostrar la alerta
+    setTimeout(function () {
       // Muestra una alerta interactiva
       Swal.fire({
         title: '¡Gracias por confirmar tu asistencia!',
-        text: 'Te redirigimos a WhatsApp asi confirmas tu asistencia 😎',
+        text: 'Te redirigimos a WhatsApp para que confirmes tu asistencia 😎',
         icon: 'success',
         confirmButtonText: 'Aceptar'
       }).then((result) => {
@@ -23,8 +25,10 @@ document.addEventListener("DOMContentLoaded", function () {
           window.location.href = 'https://api.whatsapp.com/send?phone=+5491132620544&text=Hola,%20quiero%20confirmar%20mi%20asistencia%20al%20Baby%20Shower.';
         }
       });
-    });
+    }, 2200); // 2200 ms de retraso
   });
+});
+
   
   
   
